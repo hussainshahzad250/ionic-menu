bc9cba3fff45102c567b5a941e8719b4fb00b499
Shahzad Hussain <shahzad.hussain@satincreditcare.com>
2018-11-10 21:04:19 +0530

    first commit

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,47 @@
+import { Component } from '@stencil/core';
+
+@Component({
+  tag: 'page-tabs',
+  styleUrl: 'tabs.css'
+})
+export class PageTabs {
+  render() {
+    return (
+      <ion-tabs>
+        <ion-tab selected label="Music" icon="musical-note">
+          <ion-header translucent>
+            <ion-toolbar>
+              <ion-title>Music</ion-title>
+            </ion-toolbar>
+          </ion-header>
+
+          <ion-content fullscreen padding>
+            <h1>Music</h1>
+          </ion-content>
+        </ion-tab>
+        <ion-tab label="Movies" icon="videocam">
+          <ion-header translucent>
+            <ion-toolbar>
+              <ion-title>Movies</ion-title>
+            </ion-toolbar>
+          </ion-header>
+
+          <ion-content fullscreen padding>
+            <h1>Movies</h1>
+          </ion-content>
+        </ion-tab>
+        <ion-tab label="Games" icon="logo-game-controller-b">
+          <ion-header translucent>
+            <ion-toolbar>
+              <ion-title>Games</ion-title>
+            </ion-toolbar>
+          </ion-header>
+
+          <ion-content fullscreen padding>
+            <h1>Games</h1>
+          </ion-content>
+        </ion-tab>
+      </ion-tabs>
+    );
+  }
+}
---
+++ stencil.config.ts
@@ -0,0 +1,24 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  globalStyle: 'src/global.css',
+  globalScript: 'src/global.js',
+
+  outputTargets: [
+    {
+      type: 'www',
+      serviceWorker: null,
+      dir: 'www',
+    },
+    {
+      type: 'www',
+      baseUrl: '/docs/content/component-preview-app/docs-www',
+      serviceWorker: null,
+      dir: 'docs-www',
+
+      // workaround, global CSS styles URL is wrong
+      inlineStyles: true,
+      inlineAssetsMaxSize: 30000
+    }
+  ],
+};
